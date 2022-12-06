69b860491d09410225e3cd1d599332e7b85e247c
Amshuman Rao Karaya <amshuman.kr@gmail.com>
2022-12-06 14:17:36 +0530

    Completed section ಸರ್ವನಾಮಗಳ ಪ್ರಥಮಾ ವಿಭಕ್ತಿಯಲ್ಲೂ, ಗುಣವಾಚಕ, ನಿರ್ದೇಶನಾತ್ಮಕ ರೂಪಗಳಲ್ಲೂ ಕಾಣುವ ಆದಿಯ ದೀರ್ಘಸ್ವರ Split ಪೂರ್ವವಪಕ್ಷ.

--- hack/print-readme-content.sh
+++ hack/print-readme-content.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-ls -1t content \
-    | grep "\.md$" \
-    | sed 's/\(.*\)\.\(.*\)/- [\1](content\/\1.\2)/' \
-    | sed 's/\((.*\) \(.*)\)/\1%20\2/g'
+function article_name() {
+    basename --suffix ".md" "$1"
+}
+
+function escape_space() {
+    echo "$*" | sed 's/ /%20/g'
+}
+
+ls -1t ./content | while read -r i; do
+        ia=$(article_name "$i")
+        ie=$(escape_space "$i")
+
+        echo "- [$ia](content/$ie)"
+
+        if [ -d "content/$i" ]; then
+            ls -1 "content/${i}" | grep '.md$' | while read -r j; do
+                ja=$(article_name "$j")
+                je=$(escape_space "$j")
+                
+                echo -e "\t - [$ja](content/$ie/$je)"
+            done
+       fi
+    done
+
+# ls -1t content \
+    # | grep "\.md$" \
+    # | sed 's/\(.*\)\.\(.*\)/- [\1](content\/\1.\2)/' \
+    # | sed 's/\((.*\) \(.*)\)/\1%20\2/g'
